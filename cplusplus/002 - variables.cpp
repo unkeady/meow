ab4ddf263c4b48f21e8acb6b50a44c0ed273d0af
Meow <65081668+unkeady@users.noreply.github.com>
2020-05-18 16:08:06 +0300

    Create 002 - variables.cpp

---
+++ cplusplus/002 - variables.cpp
@@ -0,0 +1,23 @@
+/* c++ programlama değişken tipleri */
+#include <iostream>
+using namespace std;
+int main()
+{
+int a = 32;
+float b = 3.52;
+double c = 34.2345; //daha büyük değer tutabilirsin.
+char d = 'A'; // ASCII
+bool e = true;
+
+cout << "A değişkeninin değeri: " << a << endl;
+
+cout << "B değişkeninin değeri: " << b << endl;
+
+cout << "C değişkeninin değeri: " << c << endl;
+
+cout << "D değişkeninin değeri: " << d << endl;
+
+cout << "E değişkeninin değeri: " << e << endl;
+
+return 0;
+}
