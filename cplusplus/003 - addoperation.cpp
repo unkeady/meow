e5a6dc1d46c997f90ad2f154ad1aacab6dad7ada
Meow <65081668+unkeady@users.noreply.github.com>
2020-05-18 16:11:38 +0300

    Create 003 - addoperation.cpp

---
+++ cplusplus/003 - addoperation.cpp
@@ -0,0 +1,12 @@
+/* C++ programlamada basit bir toplama işlemi */
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 2;
+    int b = 4;
+    int c = 6;
+    int toplam = a + b + c;
+    cout << "Toplam değer: " << toplam << endl;
+    return 0;
+}
