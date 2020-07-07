12460235da1b9e07e6e43a3079255bed0a5d36de
unkeady <unkeady@gmail.com>
2020-07-07 16:25:09 +0300

    py

--- python/helloworld.py
+++ python/helloworld.py
@@ -1,1 +1,6 @@
-print("Hello World")
+print("Hello, world")
+
+x = 1
+
+if x == 1:
+    print("Goodbye, world..")
--- python/variables.py
+++ python/variables.py
@@ -1,8 +1,27 @@
-int = 5
-print(int)
+int1 = 5
+print(int1)
 
 float1 = 7.1
 print(float1)
 
 float2 = float(7.2)
 print(float2)
+
+string1 = "Flying" # " veya ' kullanılabiliyor sanırım.
+print(string1)
+
+string2 = "Seems like your trying to bring me down"
+print(string2)
+
+hello = "hello"
+world = "world"
+
+helloworld = hello + " " + world
+
+print(helloworld)
+
+a,b = 2,3
+print(a,b)
+
+if string1 == "Flying":
+    print("Song name: %s" %string1)
