88e2ecbebaef84c5e9f839d97dc1eb03af252cb7
unkeady <unkeady@gmail.com>
2020-07-07 19:09:32 +0300

    py

---
+++ python/operators.py
@@ -0,0 +1,20 @@
+number = 1 + 2 * 3 / 4.0
+print(number)
+
+mod = 11 % 3
+print(mod)
+
+squared = 7 ** 2
+cubed = 2 ** 3
+print(squared)
+print(cubed)
+
+lotsofhellos = "hello " * 10
+print(lotsofhellos)
+
+print([1,2,3] * 3)
+
+even_numbers = [2,4,6,8]
+odd_numbers = [1,3,5,7]
+all_numbers = odd_numbers + even_numbers
+print(all_numbers)
