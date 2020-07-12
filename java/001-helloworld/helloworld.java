3104d4435c86191be625ab8d5054793126a80c13
unkeady <unkeady@gmail.com>
2020-07-12 17:31:33 +0300

    12.07

---
+++ java/001-helloworld/helloworld.java
@@ -0,0 +1,12 @@
+public class helloworld
+{
+
+    public static void main(String[] args)
+    {
+
+        System.out.print("Ehehehe");
+        System.out.println("Ecchi seven de ne bilim.s"); //sout kısayolu
+        
+    }
+    
+}
